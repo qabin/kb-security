16084ad6671beb5e35c551b2223d1725a43dbbc6
kongbin02 <kongbin02@ppdai.com>
2019-08-01 22:12:08 +0800

    init

--- kb-security-backend/server/src/main/resources/db/tables.sql
+++ kb-security-backend/server/src/main/resources/db/tables.sql
@@ -14,8 +14,9 @@ CREATE TABLE `kb-security`.`chat_info` (
   `login_name` varchar(128) NOT NULL COMMENT '登录名',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `praise_count` int(11) NOT NULL DEFAULT '0' COMMENT '点赞数量',
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `kb-security`.`user_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -24,12 +25,10 @@ CREATE TABLE `kb-security`.`user_info` (
   `encrypt_type` int(11) NOT NULL COMMENT '加密方式：\\n1. 不加密 2. Base64加密 3. md5加密 4.aes加密',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '修改时间',
-  `phone` varchar(128) DEFAULT NULL COMMENT '手机号',
-  `description` varchar(256) DEFAULT NULL COMMENT '描述',
   `user_type` int(11) NOT NULL DEFAULT '1' COMMENT '1. 普通用户 2. 管理员',
   PRIMARY KEY (`id`),
   UNIQUE KEY `login_name_UNIQUE` (`login_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `kb-security`.`xss_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -38,9 +37,12 @@ CREATE TABLE `kb-security`.`xss_info` (
   `cookie` varchar(1024) DEFAULT NULL COMMENT 'Cookie',
   `ip` varchar(256) DEFAULT NULL COMMENT 'Ip地址',
   `img` longtext COMMENT '截图',
-  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_time` datetime NOT NULL COMMENT '创建时间',
   `user_agent` varchar(1024) DEFAULT NULL COMMENT '用户终端信息',
   `command` varchar(1024) DEFAULT NULL COMMENT '命令',
   `type` int(11) DEFAULT NULL COMMENT '类型： 1. img 2.screen 3. command',
+  `user_id` int(11) NOT NULL COMMENT '用户id',
+  `status` int(11) NOT NULL DEFAULT '2' COMMENT '1. 连接中   2 .断开',
+  `update_time` datetime NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8;
